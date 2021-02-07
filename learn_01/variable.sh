bfd241e263421487e5066849a134aaf50df18145
tudou <tudou@3dmario.com>
2021-02-07 20:18:09 +0800

    增加变量练习脚本

---
+++ learn_01/variable.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+var="this is a new world!"
+echo $var	#this is a new world!
+
+echo "Test: $var"	#Test: this is a new world!
+echo "Test2: ${var}" #Test2: this is a new world!
+
+
+#let var2="this is test variable" //  syntax error in expression (error token is "is test variable")
+let var2=1000
+echo $var2	#1000
+
+var3="this is the third variable!"
+
+echo $var $var2 $var3	#this is a new world! 1000 this is the third variable!
+echo "$var $var2 $var3"	#this is a new world! 1000 this is the third variable!
+echo '$var $var2 $var3'	#$var $var2 $var3
+
+echo $var : $var2 : $var3	#this is a new world! : 1000 : this is the third variable!
+echo "$var : $var2 : $var3"	#this is a new world! : 1000 : this is the third variable!
+echo '$var : $var2 : $var3'	#$var : $var2 : $var3
+
+
+#变量合成变量
+for var4 in 1 2 3
+do
+	echo $var4
+	var5=var${var4}
+	echo $var5
+#	echo ${$var5}	#  ${$var5}: bad substitution
+
+done
+
+#入参
+echo $0
+echo $1
+echo $2
+
+#系统变量
+echo BASH$BASH
+echo HOME$HOME
+echo HOSTNAME:$HOSTNAME
+echo LANG:$LANG
+echo SECONDES:$SECONDS
+echo SHELL:$SHELL
+echo PATH:$PATH
